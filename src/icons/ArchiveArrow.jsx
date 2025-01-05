3a1c86af5683068dc14ac68ad6afb1ebd5487a57
devsany <sunnyrajbodhgaya19@gmail.com>
2025-01-05 12:44:13 +0530

    add svg

---
+++ src/icons/ArchiveArrow.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ArchiveArrow = ({ size = 24, color = "currentColor" }) => (
+  <svg
+    class="w-6 h-6 text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M4 4a2 2 0 1 0 0 4h16a2 2 0 1 0 0-4H4Zm0 6h16v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-8Zm10.707 5.707a1 1 0 0 0-1.414-1.414l-.293.293V12a1 1 0 1 0-2 0v2.586l-.293-.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l2-2Z"
+      clip-rule="evenodd"
+    />
+  </svg>
+);
+
+export default ArchiveArrow;
